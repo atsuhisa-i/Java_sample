5ac194bf5c000903f3b5a99aea88020b1ae095f7
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-23 22:17:45 +0900

    Javaの基本2

--- Sample1.java
+++ Sample1.java
@@ -2,7 +2,7 @@ class Sample1
 {
   public static void main(String[] args)
   {
-    System.out.println("ようこそJavaへ！");
-    System.out.println("Javaを始めましょう！");
+    System.out.print("ようこそJavaへ！");
+    System.out.print("Javaを始めましょう！");
   }
 }
