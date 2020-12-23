21a4331bae79717eb173032e680e8bc962a739aa
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-23 23:16:30 +0900

    Javaの基本4

---
+++ Sample4.java
@@ -0,0 +1,9 @@
+class Sample4
+{
+  public static void main(String[] args)
+  {
+    // バックスラッシュ二回でバックスラッシュを表示する。¥を表示したい場合は¥を一回記載。
+    System.out.println("円記号を表示します。:\\");
+    System.out.println("アポストロフィを表示します。:\'");
+  }
+}
