3ddf3935f98a6776010604d44ed15ea5c8729a2c
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-25 22:16:02 +0900

    変数6

--- Sample6.java
+++ Sample6.java
@@ -1,10 +1,21 @@
+import java.io.*;
+
 class Sample6
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws IOException
   {
-    System.out.println("10進数の10は" + 10 + "です。");
-    System.out.println("8進数の10は" + 010 + "です。");
-    System.out.println("16進数の10は" + 0x10 + "です。");
-    System.out.println("16進数のFは" + 0xF + "です。");
+    System.out.println("整数を2つ入力して下さい。");
+
+    BufferedReader br =
+      new BufferedReader(new InputStreamReader(System.in));
+    
+    String str1 = br.readLine();
+    String str2 = br.readLine();
+
+    int num1 = Integer.parseInt(str1);
+    int num2 = Integer.parseInt(str2);
+
+    System.out.println("最初に" + num1 + "が入力されました。");
+    System.out.println("次に" + num2 + "が入力されました。");
   }
 }
