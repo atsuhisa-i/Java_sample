dfcaef6f11b5239a7f30964696ab023bb278a1ac
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-27 23:02:28 +0900

    式と演算子6

--- Sample5.java
+++ Sample5.java
@@ -5,8 +5,8 @@ class Sample5
     int a = 0;
     int b = 0;
 
-    b = a++;
-    System.out.println("代入後にインクリメントしたのでbの値は" + b + "です。");
+    b = ++a;
+    System.out.println("代入前にインクリメントしたのでbの値は" + b + "です。");
 
   }
 }
