13647434fda9574e01de1179ade9bb6b151c64e5
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-28 21:22:49 +0900

    場合に応じた処理1

--- Sample1.java
+++ Sample1.java
@@ -1,8 +1,20 @@
+import java.io.*;
+
 class Sample1
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws IOException
   {
-    System.out.println("1+2は" + (1+2) + "です。");
-    System.out.println("3*4は" + (3*4) + "です。");
+    System.out.println("整数を入力して下さい。");
+    
+    BufferedReader br =
+      new BufferedReader(new InputStreamReader(System.in));
+    
+    String str = br.readLine();
+    int res = Integer.parseInt(str);
+
+    if(res == 1)
+      System.out.println("1が入力されました。");
+    
+    System.out.println("処理を終了します。");
   }
 }
