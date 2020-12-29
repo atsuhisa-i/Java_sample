f0e4eada488895382561432dc1166ed2632f6570
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-29 22:08:22 +0900

    場合に応じた処理10

--- Practice2.java
+++ Practice2.java
@@ -4,15 +4,25 @@ class Practice2
 {
   public static void main(String[] args) throws IOException
   {
-    System.out.println("円周率の値はいくつですか？");
+    System.out.println("2つの整数を入力して下さい。");
 
     BufferedReader br =
       new BufferedReader(new InputStreamReader(System.in));
     
-    String str = br.readLine();
+    String str1 = br.readLine();
+    String str2 = br.readLine();
 
-    float num = Float.parseFloat(str);
+    int res1 = Integer.parseInt(str1);
+    int res2 = Integer.parseInt(str2);
 
-    System.out.println("円周率の値は" + num + "です。");
+    if(res1 == res2)
+    {
+      System.out.println("2つの数は同じ値です。");
+    }else if(res1 < res2)
+    {
+      System.out.println(res1 + "より" + res2 + "の方が大きい値です。");
+    }else{
+      System.out.println(res1 + "の方が" + res2 + "より大きい値です。");
+    }
   }
 }
