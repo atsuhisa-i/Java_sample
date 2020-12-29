2d54976ceed0ce31f89f2c53c875970ed5ce0e3f
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-29 22:17:36 +0900

    場合に応じた処理11

--- Practice3.java
+++ Practice3.java
@@ -4,21 +4,21 @@ class Practice3
 {
   public static void main(String[] args) throws IOException
   {
-    System.out.println("身長と体重を入力して下さい。");
+    System.out.println("0から10までの整数を入力して下さい。");
 
     BufferedReader br =
       new BufferedReader(new InputStreamReader(System.in));
     
-    System.out.println("身長");
-    String str1 = br.readLine();
+    String str = br.readLine();
 
-    System.out.println("体重");
-    String str2 = br.readLine();
+    int res = Integer.parseInt(str);
 
-    Float num1 = Float.parseFloat(str1);
-    Float num2 = Float.parseFloat(str2);
-
-    System.out.println("身長は" + num1 + "cmです。");
-    System.out.println("体重は" + num2 + "kgです。");
+    if(res >= 0 && res <= 10 )
+    {
+      System.out.println("正解です。");
+    }
+    else{
+      System.out.println("間違いです。");
+    }
   }
 }
