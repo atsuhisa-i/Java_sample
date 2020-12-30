27ede8a82d33466560152f67f8b75065ecbf47a6
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-30 14:05:15 +0900

    何度も繰り返す16

--- Practice5.java
+++ Practice5.java
@@ -4,16 +4,22 @@ class Practice5
 {
   public static void main(String[] args) throws IOException
   {
-    System.out.println("正方形の辺の長さを入力して下さい。");
+    System.out.println("2以上の整数を入力して下さい。");
 
     BufferedReader br = 
       new BufferedReader(new InputStreamReader(System.in));
     
     String str = br.readLine();
-
     int num = Integer.parseInt(str);
-    int ans = num*num;
-
-    System.out.println("正方形の面積は" + ans + "です。");
+    
+    for(int i=2; i<=num; i++){
+      if(i == num){
+        System.out.println(num + "は素数です。");
+      }
+      else if(num % i == 0){
+        System.out.println(num + "は素数ではありません。");
+        break;
+      }
+    }
   }
 }
