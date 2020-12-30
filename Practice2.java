ef7c145d3f47294e648c9d8ee26fea225ec00cac
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-30 13:28:45 +0900

    何度も繰り返す13

--- Practice2.java
+++ Practice2.java
@@ -4,25 +4,20 @@ class Practice2
 {
   public static void main(String[] args) throws IOException
   {
-    System.out.println("2つの整数を入力して下さい。");
+    System.out.println("テストの点数を入力して下さい。(0で終了)");
 
     BufferedReader br =
       new BufferedReader(new InputStreamReader(System.in));
+
+    int num = 0;
+    int sum = 0;
+
+    do{
+      String str = br.readLine();
+      num = Integer.parseInt(str);
+      sum += num;
+    }while(num != 0);
     
-    String str1 = br.readLine();
-    String str2 = br.readLine();
-
-    int res1 = Integer.parseInt(str1);
-    int res2 = Integer.parseInt(str2);
-
-    if(res1 == res2)
-    {
-      System.out.println("2つの数は同じ値です。");
-    }else if(res1 < res2)
-    {
-      System.out.println(res1 + "より" + res2 + "の方が大きい値です。");
-    }else{
-      System.out.println(res1 + "の方が" + res2 + "より大きい値です。");
-    }
+    System.out.println("テストの合計点は" + sum + "点です。");
   }
 }
