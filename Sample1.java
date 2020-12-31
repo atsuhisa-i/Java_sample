e4171bb40a97da5c7ca9d9d59c944e4f6e0ed900
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-31 13:04:35 +0900

    配列1

--- Sample1.java
+++ Sample1.java
@@ -2,9 +2,17 @@ class Sample1
 {
   public static void main(String[] args)
   {
-    for(int i=1; i<=5; i++){
-      System.out.println("繰り返しています。");
+    int[] test;
+    test = new int[5];
+
+    test[0] = 80;
+    test[1] = 60;
+    test[2] = 22;
+    test[3] = 50;
+    test[4] = 75;
+
+    for(int i=0; i<=5; i++){
+      System.out.println((i+1) + "番目の人の点数は" + test[i] + "です。");
     }
-    System.out.println("繰り返しが終わりました。");
   }
 }
