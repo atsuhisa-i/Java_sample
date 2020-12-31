86e8eff5c48441f528b0011f3929e2452151e0c4
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-31 13:16:43 +0900

    配列2

--- Sample2.java
+++ Sample2.java
@@ -1,17 +1,30 @@
+import java.io.*;
+
 class Sample2
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws IOException
   {
-    for(int i=1; i<=5; i++){
-      System.out.println(i + "番目の繰り返しです。");
-    }
-    System.out.println("繰り返しが終わりました。");
+    System.out.println("テストの受験者数を入力して下さい。");
 
-    // for文のブロック外でiを使用したい場合はfor文を始める前にiを宣言する。
-    int i;
-    for(i=1; i<=5; i++){
-      System.out.println(i + "番目の繰り返しです。");
+    BufferedReader br =
+      new BufferedReader(new InputStreamReader(System.in));
+
+    String str = br.readLine();
+    int num = Integer.parseInt(str);
+
+    int[] test;
+    test = new int[num];
+
+    System.out.println("人数分の点数を入力して下さい。");
+
+    for(int i=0; i<num; i++){
+      str = br.readLine();
+      int tmp = Integer.parseInt(str);
+      test[i] = tmp;
     }
-    System.out.println((i-1) + "回繰り返しました。");
+
+    for(int i=0; i<num; i++){
+      System.out.println((i+1) + "番目の人の点数は" + test[i] + "です。");
+    }
   }
 }
