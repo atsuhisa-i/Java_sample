1f02cc95e0c876ac9771c84495473b2c9f627915
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-01-01 19:22:42 +0900

    クラスの基本3

--- Sample3.java
+++ Sample3.java
@@ -1,17 +1,31 @@
+class Car
+{
+  int num;
+  double gas;
+
+  void show()
+  {
+    System.out.println("車のナンバーは" + num + "です。");
+    System.out.println("ガソリン量は" + gas + "です。");
+  }
+  void showCar()
+  {
+    System.out.println("これから車の情報を表示します。");
+    // 下記の自分自身のメソッドにはshow()の前にthis.をつけることも可能。
+    show();
+  }
+}
+
 class Sample3
 {
   public static void main(String[] args)
   {
-    int[] test = new int[5];
+    Car car1;
+    car1 = new Car();
 
-    test[0] = 80;
-    test[1] = 60;
-    test[2] = 22;
-    test[3] = 50;
-    test[4] = 75;
+    car1.num = 1234;
+    car1.gas = 20.5;
 
-    for(int i=0; i<5; i++){
-      System.out.println((i+1) + "番目の人の点数は" + test[i] + "です。");
-    }
+    car1.showCar();
   }
 }
