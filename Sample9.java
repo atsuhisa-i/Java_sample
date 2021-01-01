1bd564f88142480852ed7cbeed654e8759ac1a80
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-01-01 09:10:47 +0900

    配列9

--- Sample9.java
+++ Sample9.java
@@ -4,18 +4,29 @@ class Sample9
 {
   public static void main(String[] args)throws IOException
   {
-    System.out.println("何番目でループを中止しますか？(1~10)");
-    
     BufferedReader br =
       new BufferedReader(new InputStreamReader(System.in));
-    
-    String str = br.readLine();
-    int res = Integer.parseInt(str);
 
-    for(int i=1; i<=10; i++){
-      System.out.println(i + "番目の処理です。");
-      if(i == res)
-        break;
+    int[] test = new int[5];
+    System.out.println(test.length + "人の点数を入力して下さい。");
+
+    for(int i=0; i<test.length; i++){
+      String str = br.readLine();
+      test[i] = Integer.parseInt(str);
+    }
+
+    for(int s=0; s<test.length-1; s++){
+      for(int t=s+1; t<test.length; t++){
+        if(test[t] > test[s]){
+          int tmp = test[t];
+          test[t] = test[s];
+          test[s] = tmp;
+        }
+      }
+    }
+
+    for(int j=0; j<test.length; j++){
+      System.out.println((j+1) + "番目の人の点数は" + test[j] + "です。");
     }
   }
 }
