a92f65803eb02a84ce68830d97e9c1106315de3d
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-01-06 22:44:16 +0900

    クラスの利用11

--- Practice4.java
+++ Practice4.java
@@ -1,12 +1,26 @@
+import java.io.*;
+
 class Practice4
 {
-  public static void main(String[] args)
+  public static void main(String[] args)throws IOException
   {
-    for(int i=1; i<=5; i++){
-      for(int j=0; j<i; j++){
-        System.out.print("*");
-      }
-      System.out.print("\n");
-    }
+    System.out.println("文字列を入力して下さい。");
+
+    BufferedReader br =
+      new BufferedReader(new InputStreamReader(System.in));
+    
+    String str = br.readLine();
+
+
+    System.out.println("aの挿入位置を整数で入力して下さい");
+
+    String str2 = br.readLine();
+    int num = Integer.parseInt(str2);
+
+    String strin = "a";
+    StringBuffer sb = new StringBuffer(str);
+    sb.insert(num, strin);
+
+    System.out.println(sb + "になりました。");
   }
 }
