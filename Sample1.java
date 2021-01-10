caf48eac428cff16ee80ed4568aa828943257411
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-01-10 16:23:40 +0900

    例外と入出力処理1

--- Sample1.java
+++ Sample1.java
@@ -2,7 +2,19 @@ class Sample1
 {
   public static void main(String[] args)
   {
-    Car car1 = new Car();
-    car1.show();
+    try{
+      int[] test;
+      test = new int[5];
+
+      System.out.println("test[10]に値を代入します。");
+
+      test[10] = 80;
+      System.out.println("test[10]に80を代入しました。");
+    }
+    catch(ArrayIndexOutOfBoundsException e){
+
+      System.out.println("配列の要素を超えています。");
+    }
+    System.out.println("無事終了しました。");
   }
 }
