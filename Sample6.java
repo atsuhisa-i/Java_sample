d237105861ce490e26619c05c7efe9ac98e94d48
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-01-14 21:54:44 +0900

    グラフィカルなアプリケーション6

--- Sample6.java
+++ Sample6.java
@@ -1,29 +1,45 @@
-class Car implements Runnable
-{
-  private String name;
+import java.awt.*;
+import java.awt.event.*;
 
-  public Car(String nm)
-  {
-    name = nm;
-  }
-  public void run()
-  {
-    for(int i=0; i<5; i++){
-      System.out.println(name + "の処理をしています。");
-    }
-  }
-}
-
-class Sample6
+public class Sample6 extends Frame
 {
+  int x = 10;
+  int y = 10;
+
   public static void main(String[] args)
   {
-    Car car1 = new Car("1号車");
-    Thread th1 = new Thread(car1);
-    th1.start();
+    Sample6 sm = new Sample6();
+  }
+  public Sample6()
+  {
+    super("サンプル");
 
-    for(int i=0; i<5; i++){
-      System.out.println("main()の処理をしています。");
+    addWindowListener(new SampleWindowListener());
+    addMouseListener(new SampleMouseAdapter());
+
+    setSize(250, 200);
+    setVisible(true);
+  }
+  public void paint(Graphics g)
+  {
+    g.setColor(Color.RED);
+    g.fillOval(x, y, 10,10);
+  }
+
+  class SampleWindowListener extends WindowAdapter
+  {
+    public void windowClosing(WindowEvent e)
+    {
+      System.exit(0);
+    }
+  }
+  class SampleMouseAdapter extends MouseAdapter
+  {
+    public void mousePressed(MouseEvent e)
+    {
+      x = e.getX();
+      y = e.getY();
+      repaint();
     }
   }
 }
